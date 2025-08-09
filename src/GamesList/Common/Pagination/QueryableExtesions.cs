f4e3ada6358beb690b02b4facb1a666f57af0352
Ruan Sousa <ruansousad4c@gmail.com>
2025-08-09 18:07:51 -0300

    feat: criação de classes para implementação de paginação

---
+++ src/GamesList/Common/Pagination/QueryableExtesions.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesList.Common.Pagination
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PagedResult<T>> ToPagedResultAsync<T>(
+        this IQueryable<T> query,
+        PaginationParams paginationParams)
+        {
+            var totalItems = await query.CountAsync();
+            var items = await query.Skip((paginationParams.Page - 1) * paginationParams.PageSize)
+            .Take(paginationParams.PageSize).ToListAsync();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                TotalItems = totalItems,
+                Page = paginationParams.Page,
+                PageSize = paginationParams.PageSize
+            };
+        }
+
+    }
+}
